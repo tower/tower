950c0e72ef655a144dcf296c2ed048a18ef10524
Lance Pollard <lancejpollard@gmail.com>
2012-09-18 15:22:09 -0700

    quotes

--- test/example/server.js
+++ test/example/server.js
@@ -1,12 +1,12 @@
 // node test/example/server --static true
 process.env.TOWER_ROOT = __dirname;
 
-require("coffee-script");
-require("../..");
+require('coffee-script');
+require('../..');
 
-Tower.View.engine = "coffee"
+Tower.View.engine = 'coffee'
 // must run `node server` relative to this test/example folder
-Tower.View.store().loadPaths = ["app/templates/shared", "app/templates/server"]
+Tower.View.store().loadPaths = ['app/templates/shared', 'app/templates/server']
 
 Tower.watch = false
 
