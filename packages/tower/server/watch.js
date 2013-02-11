3e9721ec3c6631029a3b15bdefbb503afb929cc4
Daniel Fagnan <dnfagnan@gmail.com>
2013-02-10 21:12:35 -0700

    converted to spaces
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- packages/tower/server/watch.js
+++ packages/tower/server/watch.js
@@ -5,147 +5,147 @@ var gaze = require('gaze');
 var Gaze = gaze.Gaze;
 
 function Watch(paths) {
-	// Just to get a cleaner syntax:
-	if(!(this instanceof Watch)) {
-		return new Watch(paths);
-	}
+    // Just to get a cleaner syntax:
+    if(!(this instanceof Watch)) {
+        return new Watch(paths);
+    }
 
-	this.isWatching = false;
+    this.isWatching = false;
 
-	// Defaulted Options:
-	var options = {
-		paths: [],
-		ignore: [],
-		filters: [],
-		events: {
-			'changed': [],
-			'removed': [],
-			'added': [],
-			'ready': [],
-			'error': []
-		},
-		forcePolling: false,
-		latency: null
-	};
+    // Defaulted Options:
+    var options = {
+        paths: [],
+        ignore: [],
+        filters: [],
+        events: {
+            'changed': [],
+            'removed': [],
+            'added': [],
+            'ready': [],
+            'error': []
+        },
+        forcePolling: false,
+        latency: null
+    };
 
-	if (typeof paths == "string") {
-		options.paths.push(paths);
-	} else {
-		for (var i = 0; i<paths.length; i++) {
-			options.paths.push(paths[i]);
-		}
-	}
+    if (typeof paths == "string") {
+        options.paths.push(paths);
+    } else {
+        for (var i = 0; i<paths.length; i++) {
+            options.paths.push(paths[i]);
+        }
+    }
 
-	this.options = options;
+    this.options = options;
 
-	return this;
+    return this;
 }
 
 Watch.prototype.ignore = function(regex) {
 
-	if(regex instanceof Array) {
-		for(var i = 0; i < regex.length; i++) {
-			this.options.ignore.push(regex[i]);
-		}
-	} else {
-		this.options.ignore.push(regex);
-	}
+    if(regex instanceof Array) {
+        for(var i = 0; i < regex.length; i++) {
+            this.options.ignore.push(regex[i]);
+        }
+    } else {
+        this.options.ignore.push(regex);
+    }
 
-	return this;
+    return this;
 };
 
 Watch.prototype.latency = function(num) {
-	this.options.latency = num;
-	return this;
+    this.options.latency = num;
+    return this;
 };
 
 Watch.prototype.filter = function(regex) {
 
-	if(regex instanceof Array) {
-		for(var i = 0; i < regex.length; i++) {
-			this.options.filters.push(regex[i]);
-		}
-	} else {
-		this.options.filters.push(regex);
-	}
+    if(regex instanceof Array) {
+        for(var i = 0; i < regex.length; i++) {
+            this.options.filters.push(regex[i]);
+        }
+    } else {
+        this.options.filters.push(regex);
+    }
 
-	return this;
+    return this;
 };
 
 Watch.prototype.forcePolling = function(bool) {
-	this.options.forcePolling = bool;
-	return this;
+    this.options.forcePolling = bool;
+    return this;
 }
 
 Watch.prototype.start = function(callback) {
-	var self = this;
-	// XXX: Wondering if we should create multiple watchers
-	// 		for multiple paths. 
-	this._instance = new Gaze(this.options.paths[0], {maxListeners:0});
+    var self = this;
+    // XXX: Wondering if we should create multiple watchers
+    //      for multiple paths. 
+    this._instance = new Gaze(this.options.paths[0], {maxListeners:0});
 
-	this._instance.on('ready', function(w) {
-		self.emit('ready');
-		self.isWatching = true;
-	});
+    this._instance.on('ready', function(w) {
+        self.emit('ready');
+        self.isWatching = true;
+    });
 
-	this._instance.on('changed', function(filepath) {
-		self.emit('changed', [filepath]);
-	});
+    this._instance.on('changed', function(filepath) {
+        self.emit('changed', [filepath]);
+    });
 
-	this._instance.on('added', function(filepath) {
-		self.emit('added', [filepath]);
-	});
+    this._instance.on('added', function(filepath) {
+        self.emit('added', [filepath]);
+    });
 
-	this._instance.on('deleted', function(filepath) {
-		self.emit('removed', [filepath]);
-	});
+    this._instance.on('deleted', function(filepath) {
+        self.emit('removed', [filepath]);
+    });
 
-	this._instance.on('all', function(filepath) {
-		self.emit('all', [filepath]);
-	});
+    this._instance.on('all', function(filepath) {
+        self.emit('all', [filepath]);
+    });
 
-	this._instance.on('error', function(err) {
-		self.emit('error', [err]);
-	});
+    this._instance.on('error', function(err) {
+        self.emit('error', [err]);
+    });
 }
 
 Watch.prototype.emit = function(event, args) {
-	if(!event) return false;
+    if(!event) return false;
 
-	if (this.options.events[event] == null) 
-		return false;
+    if (this.options.events[event] == null) 
+        return false;
 
-	for(var i = 0; i < this.options.events[event].length; i++) {
-		if(typeof this.options.events[event][i] == "function") {
-			this.options.events[event][i].apply({}, args);
-		}
-	}
+    for(var i = 0; i < this.options.events[event].length; i++) {
+        if(typeof this.options.events[event][i] == "function") {
+            this.options.events[event][i].apply({}, args);
+        }
+    }
 };
 
 Watch.prototype.on = function(event, callback) {
-	this.options.events[event].push(callback);
-	return this;
+    this.options.events[event].push(callback);
+    return this;
 }
 
 /**
 Watch(path.join(Tower.path, 'packages', '**', '*'))
-	.ignore(/node_modules/)
-	.filter(/Test\.js$/)
-	.latency(0.2)
-	.forcePolling(false)
-	.on('changed', function(files) {
-		console.log(files);
-	})
-	.on('removed', function(files) {})
-	.on('added', function(files) {
-		console.log(files);
-	})
-	.on('ready', function(){
-		console.log("System is ready.");
-	})
-	.on('error', function(error) {
-		console.log("Error", error);
-	})	
-	.start();**/
+    .ignore(/node_modules/)
+    .filter(/Test\.js$/)
+    .latency(0.2)
+    .forcePolling(false)
+    .on('changed', function(files) {
+        console.log(files);
+    })
+    .on('removed', function(files) {})
+    .on('added', function(files) {
+        console.log(files);
+    })
+    .on('ready', function(){
+        console.log("System is ready.");
+    })
+    .on('error', function(error) {
+        console.log("Error", error);
+    })  
+    .start();**/
 
 module.exports = Watch;
