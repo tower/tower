d4af47c8b4a808147e5478640e14a56e9aad8644
Daniel Fagnan <dnfagnan@gmail.com>
2013-02-13 22:25:56 -0700

    start commenting like crazy :)
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// This is the main entry point for any Tower application.
+// This file exposes a function like `module.exports = function(){}`.
+// To run the function you must call `require('tower')()`.
+//
+// Let's wrap all the code to avoid leaks.
 (function() {
-    var options, _, fs, util, path, program, spawn, http, httpProxy, Status, Tower;
+    // Define some top level variables. All variables should be
+    // defined at the top of each function.
+    var options, _, fs, util, path, program, spawn, http, httpProxy, Status, Tower, last_color;
     // Initialize variables and core modules:
     fs = require('fs'), util = require('util'), path = require('path'), httpProxy = require('http-proxy'), http = require('http'), spawn = require('child_process').spawn, program = require('commander');
     // node path resolution was broken before
@@ -7,54 +14,77 @@
         require('./packages/tower-platform/path.js')
     }
 
-    var last_color = [];
-
-    // Create a small helper function:
+    // Note the last color used for logging. When we change colors
+    // we'll skip a line for clarity. Typically, you'd change colors
+    // to notify different things.
+    // XXX: Move this logging functionality inside a class
+    //      because were repeating ourselves within this file
+    //      and the sub-process.
+    last_color = [];
+    // A global log function with simple color support (you
+    // must still pass the ascii color codes in as an array or string)
     global.log = function(str, color) {
+        // Again, always defining variables at the top so it's
+        // extremely easy to find.
+        var s;
+        // If we haven't specified a color, use cyan.
         if(!color) color = '[36m';
-        var s = "";
+        // String to use to combine the array of colors.
+        s = "";
         if(color instanceof Array) {
+            // Append the `s` string with the color escape code
+            // as well as the color code itself.
             color.forEach(function(ascii) {
                 s += "\033" + ascii;
             });
         } else {
+            // It's just a string that was passed, not an array.
             s = "\033" + color;
         }
+        // If our last color is NOT the same as our built-up string `s`
+        // then skip a line (newline).
         if(last_color !== s) {
             util.print('\n');
             last_color = s;
         }
+        // Final output. We use util.print because it's raw and doesn't
+        // add newlines, which console.log does.
         util.print('\n       ::' + s + str + '\033[0m');
     };
 
+    // Define some simple default options.
     options = {
         port: 3000,
         env: 'development',
         dirname: __dirname,
+        // The current command passed to this process:
         command: null,
+        // Command arguments.
+        // We build this later on in this file.
         commandArgs: []
     };
 
-    // Mockup a simple Tower object for the error classes:
-    //Tower.HTTP = {};
-    // XXX: This will be moved into it's own file:
-    /**function CIPError() {
-        this.message = "Conflicting interal port number.";
-        this.name    = "Tower.HTTP.CIPError";
-        this.code    = 0x01;
-    }
-
-    CIP.prototype = new Error();
-    CIP.prototype.constructor = CIPError;**/
-
-    program.version('0.5.0').usage('[options]').option('-p, --port [number]', 'Port', parseInt).option('-e, --env [string]', 'Environment Type');
-
+    // Start using commander. Provide all the available options for cli
+    // commands:
+    program
+        .version('0.5.0')
+        .usage('[options]')
+        .option('-p, --port [number]', 'Port', parseInt)
+        .option('-e, --env [string]', 'Environment Type')
+        .usage('[command] [options]')
+        .on('--help', function() {
+            console.log(['Commands:', 'tower new <app-name>          generate a new Tower application in folder "app-name"', 'tower console                 command line prompt to your application', 'tower generate <generator>    generate project files (models, views, controllers, scaffolds, etc.)'].join("\n")
+            );
+    });
+    // Parse the commands.
     program.parse(process.argv);
 
+    // Store the port they specified if it's valid:
     if(program.port) {
         options.port = program.port;
     }
 
+    // Do the same with the environment value:
     if(program.env) {
         options.env = program.env;
     }
@@ -62,16 +92,22 @@
     // Export a starting function that we'll call within the
     // app's main file:
     module.exports = function(command) {
-
+        // We always want the default to be server command:
         if(!command) {
             command = 'server';
         }
-
+        // Let's build out the command arguments:
         options.commandArgs = (function() {
             var _arr = process.argv.splice(-(process.argv.length - 3), process.argv.length - 1);
             return _arr;
         })();
 
+        /**
+         * The Server class is the heart of the operation.
+         * It keeps track of the sub-process, proxy, and http server
+         * so whenever anything happens, this class will deal with it.
+         * @param {Object} options All the options.
+         */
         function Server(options) {
 
             this.options = options || {};
@@ -87,6 +123,15 @@
 
         }
 
+        /**
+         * Call this method when you want stuff to happen.
+         * This will start the proxy server (NOTE: You only really need
+         * the proxy server unless you're running the http server.)
+         * and also a simple http server.
+         * XXX: We will replace the http server with Express, but we will
+         *      move it into the sub-process.
+         * @return {void}
+         */
         Server.prototype.run = function() {
 
             // Spawn the proxy server:
@@ -101,11 +146,42 @@
             // a child of the general purpose `Error` prototype/class.
             // We could then look at the instance of the error, if it matches the previous class, then
             // we'll do the regular restart.
-            this.startHTTP();
-
         };
 
+        /**
+         * Start a simple HTTP server.
+         * XXX: This will be replaced. Only used for initial
+         *      testing of the proxy server.
+         * @return {void}
+         */
         Server.prototype.startHTTP = function() {
+
+            self.serverInstance = http.createServer(
+
+            function(req, res) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('request successfully proxied: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+                res.end();
+            });
+
+            self.serverInstance.on('error', function(err) {
+                // If we have a conflicting error then we'll need
+                // to restart the proxy server and http server on a different
+                // port.
+                if(err.code == 'EADDRINUSE') {
+                    // Kill this instance and start a new one.
+                    self.stop('Restarting the server - Conflicting Inner Port.', '[33m');
+                    self.options.inner_port++;
+                    self.run();
+                    log('Successfully restarted!', '[32m');
+                }
+            });
+
+            self.serverInstance.listen(self.options.inner_port, function() {
+                log('HTTP server has started.');
+            });
 
         };
 
@@ -184,33 +260,7 @@
             this.proxyInstance.listen(this.options.outer_port, function() {
                 log('Proxy server has started.');
                 self.startProcess();
-
-                self.serverInstance = http.createServer(
-
-                function(req, res) {
-                    res.writeHead(200, {
-                        'Content-Type': 'text/plain'
-                    });
-                    res.write('request successfully proxied: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-                    res.end();
-                });
-
-                self.serverInstance.on('error', function(err) {
-                    // If we have a conflicting error then we'll need
-                    // to restart the proxy server and http server on a different
-                    // port.
-                    if(err.code == 'EADDRINUSE') {
-                        // Kill this instance and start a new one.
-                        self.stop('Restarting the server - Conflicting Inner Port.', '[33m');
-                        self.options.inner_port++;
-                        self.run();
-                        log('Successfully restarted!', '[32m');
-                    }
-                });
-
-                self.serverInstance.listen(self.options.inner_port, function() {
-                    log('HTTP server has started.');
-                });
+                self.startHTTP();
 
             }
 
